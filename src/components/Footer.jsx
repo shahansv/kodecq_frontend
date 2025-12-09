62d1ba09ea8c1917e9568b297c5b2064c38b45c3
shahansv <shahanvsaleem@gmail.com>
2025-12-09 14:28:34 +0530

    footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { TextHoverEffect } from "./ui/TextHoverEffect";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer>
+      <div className="h-50 flex flex-col justify-between bg-black  ">
+        <TextHoverEffect text="Kodecq.dev" />
+        <div className="text-slate-700 text-sm font-bold text-center mb-2">
+          &copy; {currentYear} Kodecq.dev. All rights reserved. Developed by
+          <a
+            target="_blank"
+            href="https://github.com/shahansv"
+            className="text-lime-400/90 mx-1 font-semibold hover:text-lime-300"
+          >
+            shahan.sv
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,19 +5,19 @@ import { ArrowRightIcon } from "lucide-react";
 
 const Header = () => {
   return (
-    <div className="h-18 relative">
+    <header className="h-16 relative">
       <div className="flex justify-between items-center bg-black text-white px-8 py-4 fixed top-0 w-full">
         <Link to="/">
           <h1 className="text-2xl font-bold">
-            Kodecq<span className="text-orange-400">.dev</span>
+            Kodecq<span className="text-lime-400">.dev</span>
           </h1>
         </Link>
-        <Button className="bg-orange-400/60 border border-orange-400 font-bold hover:bg-orange-400 hover:scale-105 cursor-pointer">
+        <Button className="bg-lime-400/70 border border-lime-400 font-bold hover:bg-lime-400 hover:text-lime-900 hover:scale-105 cursor-pointer">
           Get started
           <ArrowRightIcon />
         </Button>
       </div>
-    </div>
+    </header>
   );
 };
 
