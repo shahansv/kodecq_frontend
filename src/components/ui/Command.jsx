138e1e7dbc143b28d588f03ce77672493ccc9229
shahansv <shahanvsaleem@gmail.com>
2025-12-29 23:58:20 +0530

    Design completed & socket.io setup

--- src/components/ui/Command.jsx
+++ src/components/ui/Command.jsx
@@ -11,7 +11,7 @@ import {
   DialogDescription,
   DialogHeader,
   DialogTitle,
-} from "@/components/ui/dialog";
+} from "@/components/ui/Dialog";
 
 function Command({ className, ...props }) {
   return (
